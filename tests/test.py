85ce2b01720862466a2ec22f5ed24016aa396433
Carl V <vogel.carl.l@gmail.com>
2023-02-15 03:26:05 +0100

    created test file

---
+++ tests/test.py
@@ -0,0 +1,21 @@
+
+import main.py
+
+
+def test_chest_parser():
+
+    """ test the chest_Content_Dict() function """
+
+    pass
+
+def test_recipe_parser():
+
+    """ test the make_Player_Food_Dict() function """
+
+    pass
+
+def test_recipe_comparer():
+
+    """ test the still_missing() function """
+
+    pass
