df71e71930fecce0c8d605f9b98d3dcab83609fd
Carl V <vogel.carl.l@gmail.com>
2023-02-15 05:05:02 +0100

    test for function still_missing()

--- tests/test.py
+++ tests/test.py
@@ -1,5 +1,5 @@
 
-import main.py
+from . import main
 
 
 def test_chest_parser():
@@ -8,14 +8,39 @@ def test_chest_parser():
 
     pass
 
+
 def test_recipe_parser():
 
     """ test the make_Player_Food_Dict() function """
 
     pass
 
+
 def test_recipe_comparer():
 
     """ test the still_missing() function """
 
-    pass
+    test_player = {"Stuff": 1,
+                   "Things": 3,
+                   "Objects": 5}
+    test_archive = {"Stuff": {"Filled": 1,
+                              "With": 5,
+                              "Things": 8},
+                    "Things": {"Very": 5,
+                               "Important": 7,
+                               "Text": 1},
+                    "Objects": {"Shorter": 3},
+                    "Different": {"Just": 2,
+                                  "Two": 2},
+                    "Unneeded": {"Some": 4,
+                                 "More": 2,
+                                 "Content": 3},
+                    "Filler": {"AlmostDone": 1,
+                               "It'sContent": 5,
+                               "CoolStuffBoi": 6},
+                    "Text": {"Its": 4,
+                             "Even": 1,
+                             "Moore": 23,
+                             "IKnowItsSpelledWrongItsIntended": 18}}
+
+    assert main.still_missing(test_player, test_archive) == ["Stuff", "Things", "Objects"]
